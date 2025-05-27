44cab9be3af1943977f034dc92131586bd9e5585
Galen Jack <galenj@nycstudents.net>
2025-05-27 10:42:51 -0400

    added two stack algorithm for processing equation

---
+++ Equation.java
@@ -0,0 +1,39 @@
+public class Equation{
+  private String eq;
+
+  public static void Equation(String s){
+    eq = s;
+
+  }
+  public static void convert(String s){
+    exp[] = s.split("(?!^)");
+    Stack<String> ops = new Stack<String>();
+    Stack<Double> vals = new Stack<Double>();
+
+    for(int i = 0; i < exp.length; i++) {
+                    String s = exp[i];
+        if (s.equals("(")) {
+        }
+        else if (s.equals("+") || s.equals("*")) {
+            ops.push(s);
+        } else if (s.equals(")")) {
+            getComp(ops, vals);
+        } else {
+            vals.push(Double.parseDouble(s));
+        }
+    }
+    getComp(ops, vals);
+    System.out.println(vals.pop());
+  }
+}
+
+
+
+private static void getComp(Stack<String> ops, Stack<Double> vals) {
+    String op = ops.pop();
+    if (op.equals("+")) {
+        vals.push(vals.pop() + vals.pop());
+    } else if (op.equals("*")) {
+        vals.push(vals.pop() * vals.pop());
+    }
+}
